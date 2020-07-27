9ce4558dbddd772eaede16a383ae9eebe89cc006
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-27 03:16:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,9 @@
 def square_array(numbers)
-  new_numbers = [1,2,3]
+  new_numbers = []
   numbers.each do |num|
+    numbers.collect
+    numbers.map
+    numbers.inject
     new_numbers << (num ** 2)
   end
   return new_numbers
